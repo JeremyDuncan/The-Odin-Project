9a3e2272657a8a319662060f68a49fdab64baf64
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-04-19 18:12:59 -0400

    updated formatting for easier reading.

--- Rock_Paper_Scissors/RockPaperScissors.js
+++ Rock_Paper_Scissors/RockPaperScissors.js
@@ -1,19 +1,9 @@
-/*
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
-
-
-At this point you should be using console.log() to display the results of each round and the winner at the end.
-Use prompt() to get input from the user. Read the docs here if you need to.
-Feel free to re-work your previous functions if you need to. Specifically, you might want to change the return value to something more useful.
-Feel free to create more “helper” functions if you think it would be useful.
-*/
+//========= Rock Paper Scissors Program =====================
 
 function computerPlay() {
   // choices that the computer has to choose from in game
   var computerChoice = ["Rock", "Paper", "Scissors"];
-  
+
   // randomly generates a number based on max (max = 3 [0-2])
   var max = 3; // [0-2]
   var randomNum = Math.floor(Math.random() * max);
@@ -23,47 +13,47 @@ function computerPlay() {
   return computerChoice[randomNum];
 }
 
+
 function playRound(playerSelection, computerSelection) {
 
   //make selection case insensitive. ie. Rock rock ROCK RoCk etc.
   var humanSelection = playerSelection.toLowerCase();
   var cpuSelection = computerSelection.toLowerCase();
   var gameResult = "";
-  
+
   // determines result based on player and computer selection. 
   // puts game result into gameResult as string.
   if (humanSelection == "rock") {
     if (cpuSelection == "rock") {
       gameResult = "It's a tie! You both selected Rock!";
     } else if (cpuSelection == "scissors") {
-        gameResult = "You Win! Rock beats Scissors!";
+      gameResult = "You Win! Rock beats Scissors!";
     } else if (cpuSelection == "paper") {
-        gameResult = "You Lose! Paper beats Rock!"; 
+      gameResult = "You Lose! Paper beats Rock!";
     }
   }
-
   if (humanSelection == "paper") {
     if (cpuSelection == "paper") {
       gameResult = "It's a tie! You both selected Paper!";
     } else if (cpuSelection == "rock") {
-        gameResult = "You Win! Paper beats Rock!";
+      gameResult = "You Win! Paper beats Rock!";
     } else if (cpuSelection == "scissors") {
-        gameResult = "You Lose! Scissors beats Paper!"; 
+      gameResult = "You Lose! Scissors beats Paper!";
     }
   }
- 
   if (humanSelection == "scissors") {
     if (cpuSelection == "scissors") {
       gameResult = "It's a tie! You both selected Scissors!";
     } else if (cpuSelection == "paper") {
-        gameResult = "You Win! Scissors beats Paper!";
+      gameResult = "You Win! Scissors beats Paper!";
     } else if (cpuSelection == "rock") {
-        gameResult = "You Lose! Rock beats Paper!"; 
+      gameResult = "You Lose! Rock beats Paper!";
     }
   }
 
- return gameResult; //return who won as a string.
+  return gameResult; //return who won as a string.
 }
+
 
 // calls game() function that plays 5 games of "Rock Paper Scissors"
 // calls playRound() and computerPlay() 
@@ -80,4 +70,3 @@ function game() {
 }
 
 game()
-
