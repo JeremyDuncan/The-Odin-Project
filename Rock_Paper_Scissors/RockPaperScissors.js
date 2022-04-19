3184081108cb0b7f65757f82ba59093f85e70cf9
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-04-19 17:15:41 -0400

    Added the game() function to loop through rounds.

--- Rock_Paper_Scissors/RockPaperScissors.js
+++ Rock_Paper_Scissors/RockPaperScissors.js
@@ -1,16 +1,4 @@
 /*
-Your game is going to play against the computer, so begin with a function called computerPlay that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. We’ll use this function in the game to make the computer’s play. Tip: use the console to make sure this is returning the expected output before moving to the next step!
-
-Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
-
-Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
-
-Important note: you want to return the results of this function call, not console.log() them. You’re going to use what you return later on, so let’s test this function by using console.log to see the results:
-
-function playRound(playerSelection, computerSelection) {
-  // your code here!
-}
-
 const playerSelection = "rock";
 const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
@@ -79,16 +67,15 @@ function playRound(playerSelection, computerSelection) {
  return gameResult; //return who won as a string.
 }
 
-
-/*
-  //returns string that declares a winner. (Not console)
-  const playerSelection = "rock";
-  const computerSelection = computerPlay();
-
-}
-computerPlay()
-
 function game() {
-  playRound()
+  var gameCount = 0;
+  while (gameCount < 5) {
+  console.log(playRound("Rock", computerPlay()))
+  gameCount++;
+  }
 }
+game()
+/*
+
+
 */
